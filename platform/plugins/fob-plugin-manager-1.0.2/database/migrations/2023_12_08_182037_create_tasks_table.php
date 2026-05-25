9854229d7ad4b07d3ed6a0db9659d392fe2cef1b
TheArena AI <soarer@thearena.dev>
2026-05-25 20:25:42 +0100

    added fob plugin upload

---
+++ platform/plugins/fob-plugin-manager-1.0.2/database/migrations/2023_12_08_182037_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if (Schema::hasTable('plugin_manager_tasks')) {
+            return;
+        }
+
+        Schema::create('plugin_manager_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('operation');
+            $table->string('command')->nullable();
+            $table->string('package')->nullable();
+            $table->string('status');
+            $table->mediumText('output')->nullable();
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('finished_at')->nullable();
+            $table->unsignedMediumInteger('peak_memory_used')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('plugin_manager_tasks');
+    }
+};
